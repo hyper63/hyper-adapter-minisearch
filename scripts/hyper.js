c405aac645b6307d2b7a03a13e5b75b37e7306b1
Tyler Hall <tyler@hyper.io>
2022-03-08 16:53:18 +0000

    chore: bump app and core in harness

--- scripts/hyper.js
+++ scripts/hyper.js
@@ -1,5 +1,5 @@
-import hyper from "https://x.nest.land/hyper@2.0.0/mod.js";
-import app from "https://x.nest.land/hyper-app-opine@1.2.7/mod.js";
+import hyper from "https://x.nest.land/hyper@3.0.0/mod.js";
+import app from "https://x.nest.land/hyper-app-opine@2.0.0/mod.js";
 
 import search from "../mod.js";
 
