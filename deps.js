e3bf1da103c013f052d038a4b5306d44b3210fce
Tyler Hall <tyler@hyper.io>
2023-05-23 20:00:14 -0400

    refator: remove nest.land imports

--- deps.js
+++ deps.js
@@ -5,7 +5,7 @@ export { default as MiniSearch } from "https://cdn.skypack.dev/minisearch@^4";
 export {
   HyperErr,
   isHyperErr,
-} from "https://x.nest.land/hyper-utils@0.1.0/hyper-err.js";
+} from "https://raw.githubusercontent.com/hyper63/hyper/hyper-utils%40v0.1.0/packages/utils/hyper-err.js";
 
 export { DB } from "https://deno.land/x/sqlite@v3.2.1/mod.ts";
 export * as path from "https://deno.land/std@0.159.0/path/mod.ts";
