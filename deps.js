0a6a33d7ab3d4b5e1f893e5a7b6593e0ced6de65
Tyler Hall <tyler@hyper.io>
2023-06-05 09:47:54 -0400

    chore: bump deps and move test suite to deno task

--- deps.js
+++ deps.js
@@ -1,11 +1,11 @@
-export * as R from 'https://cdn.skypack.dev/ramda@^0.28.0'
+export * as R from 'https://cdn.skypack.dev/ramda@^0.29.0'
 export { default as crocks } from 'https://cdn.skypack.dev/crocks@^0.12.4'
-export { default as MiniSearch } from 'https://cdn.skypack.dev/minisearch@^4'
+export { default as MiniSearch } from 'https://cdn.skypack.dev/minisearch@^6'
 
 export {
   HyperErr,
   isHyperErr,
-} from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-utils%40v0.1.0/packages/utils/hyper-err.js'
+} from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-utils%40v0.1.1/packages/utils/hyper-err.js'
 
-export { DB } from 'https://deno.land/x/sqlite@v3.2.1/mod.ts'
-export * as path from 'https://deno.land/std@0.159.0/path/mod.ts'
+export { DB } from 'https://deno.land/x/sqlite@v3.7.2/mod.ts'
+export * as path from 'https://deno.land/std@0.190.0/path/mod.ts'
